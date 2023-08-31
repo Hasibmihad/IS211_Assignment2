8d7d4b76c955ac4ef07c0c11ea5c6e3e65fd0f1a
Hasibmihad <mdhasibuddin12@gmail.com>
2023-08-31 13:40:03 +0600

    error

--- assignment2.py
+++ assignment2.py
@@ -1,4 +1,18 @@
 import urllib.request
+import logging
+from datetime import datetime
+
+
+
+def loggerSetup():
+
+    LOG_FILENAME = 'errors.log'
+    # Set up a specific logger with our desired output level
+    my_logger = logging.getLogger('assignment2')
+    my_logger.setLevel(logging.ERROR)
+    return my_logger
+
+
 
 def downloadData(url):
     with urllib.request.urlopen(url) as response:
@@ -7,36 +21,30 @@ def downloadData(url):
       return csv_data
 
 
+
+def processData(data):
+    personData={}
+    for i in range (1,len(data)-1):
+        each_data = data[i].split(',')
+        ID=each_data[0]
+        name=each_data[1]
+        stringbirthday =each_data[2]
+    #print(ID)
+    #print(name)
+        try:
+            birthday = datetime.strptime(stringbirthday, '%d/%m/%Y').date()
+            personData[int(ID)] = (name, birthday)
+        except ValueError:
+            my_logger.error(f"Error processing line #{i+1} for ID #{ID} - Invalid date: {stringbirthday}")
+
+
+def main() :
 #checking if it works........
-url = "https://s3.amazonaws.com/cuny-is211-spring2015/birthdays100.csv"
-downloadedData = downloadData(url)
-#print(downloadedData)
+    url = "https://s3.amazonaws.com/cuny-is211-spring2015/birthdays100.csv"
+    downloadedData = downloadData(url)
+    #print(downloadedData)
+    data2 = downloadedData.split('\n') #split upon new line for processing
 
-import logging
-
-LOG_FILENAME = 'errors.log'
-
-# Set up a specific logger with our desired output level
-my_logger = logging.getLogger('assignment2')
-my_logger.setLevel(logging.ERROR)
-
-
-data2 = downloadedData.split('\n')
-
-from datetime import datetime
-personData={}
-for i in range (1,len(data2)-1):
-  each_data = data2[i].split(',')
-  ID=each_data[0]
-  name=each_data[1]
-  stringbirthday =each_data[2]
-  #print(ID)
-  #print(name)
-try:
-    birthday = datetime.strptime(stringbirthday, '%d/%m/%Y').date()
-    personData[int(ID)] = (name, birthday)
-except ValueError:
-    my_logger.error(f"Error processing line #{i+1} for ID #{ID} - Invalid date: {stringbirthday}")
-  
+    
 
 
