e95e423a7be357a2895dea98e4b4ef697bf38e3c
Hasibmihad <mdhasibuddin12@gmail.com>
2023-08-31 13:24:20 +0600

    logger setup

--- assignment2.py
+++ assignment2.py
@@ -8,6 +8,14 @@ def downloadData(url):
 
 
 #checking if it works
-url = "https://s3.amazonaws.com/cuny-is211-spring2015/birthdays100.csv"
-downloadedData = downloadData(url)
-print(downloadedData)
+#url = "https://s3.amazonaws.com/cuny-is211-spring2015/birthdays100.csv"
+#downloadedData = downloadData(url)
+#print(downloadedData)
+
+import logging
+
+LOG_FILENAME = 'errors.log'
+
+# Set up a specific logger with our desired output level
+my_logger = logging.getLogger('assignment2')
+my_logger.setLevel(logging.ERROR)
